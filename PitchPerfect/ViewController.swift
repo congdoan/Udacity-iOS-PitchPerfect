0f2a75e2e04c1b8a50804a62318a48d798832cb7
Cong Doan <cong.doan@gmail.com>
2017-10-08 14:42:34 +0700

    Set up the Record Audio View Controller

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func recordAudio(_ sender: Any) {
+        recordButton.isEnabled = false
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
     }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+    }
 
 }
 
